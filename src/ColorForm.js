3f0899df784fc8fce333f5a39cbf3b4f4dfe16a3
Jonathan <jonathanrodriguez069@gmail.com>
2023-11-20 17:42:30 -0500

    colorblock, colorform added for rendering colors

---
+++ src/ColorForm.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+function ColorForm(props) {
+  let [input, setInput] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.addColor(input);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="hey, I'm typing here!"
+        />
+        <button type='submit'>Submit!</button>
+      </form>
+    </div>
+  );
+}
+
+export default ColorForm;
